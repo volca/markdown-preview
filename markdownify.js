0a83618899ade102b2ec24ebd859952a0381b10f
volca <volca@tom.com>
2012-10-13 22:43:04 +0800

    sync options

--- markdownify.js
+++ markdownify.js
@@ -1,11 +1,17 @@
 (function(document) {
 
-  // Onload, take the DOM of the page, get the markdown formatted text out and
+    var interval;
+
+    // Onload, take the DOM of the page, get the markdown formatted text out and
 	// apply the converter.
  	function makeHtml(data) {
 		var html = (new Showdown.converter()).makeHtml(data);
 		$(document.body).html(html);
 	}
+
+    function getThemeCss(theme) {
+        return chrome.extension.getURL('theme/' + theme + '.css');
+    }
 
     function setTheme(theme) {
         var link = $('#theme');
@@ -13,11 +19,28 @@
             var ss = document.createElement('link');
             ss.rel = 'stylesheet';
             ss.id = 'theme';
-            ss.href = chrome.extension.getURL('theme/' + theme + '.css');
+            ss.href = getThemeCss(theme);
             document.head.appendChild(ss);
         } else {
-            link.attr('href', 'theme/' + theme + '.css');
+            link.attr('href', getThemeCss(theme));
         }
+    }
+
+    function stopAutoReload() {
+        clearInterval(interval);
+    }
+
+    function startAutoReload() {
+        stopAutoReload();
+        interval = setInterval(function() {
+            $.ajax({
+                url : location.href, 
+                cache : false,
+                success : function(data) { 
+                    makeHtml(data); 
+                }
+            });
+        }, 3000);
     }
 
 	makeHtml(document.body.innerText);
@@ -32,16 +55,22 @@
 
     storage.get('auto_reload', function(items) {
         if(items.auto_reload) {
-            setInterval(function() {
-                $.ajax({
-                    url : location.href, 
-                    cache : false,
-                    success : function(data) { 
-                        makeHtml(data); 
-                    }
-                });
-            }, 3000);
+            startAutoReload();
         }
 	});
+
+    chrome.storage.onChanged.addListener(function(changes, namespace) {
+        for (key in changes) {
+            if(key == 'theme') {
+                setTheme(changes[key].newValue);
+            } else if(key == 'auto_reload') {
+                if(items.auto_reload) {
+                    startAutoReload();
+                } else {
+                    stopAutoReload();
+                }
+            }
+        }
+    });
 
 }(document));
