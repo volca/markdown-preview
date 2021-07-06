98c29415157b1356a6c69aa3f1526e7b0ecce73e
yanc <volcas@gmail.com>
2021-07-06 11:34:18 +0800

    Fixes #124 toc

--- js/markdownify.js
+++ js/markdownify.js
@@ -4,6 +4,7 @@
         interval,
         defaultReloadFreq = 3,
         previousText,
+        toc = [],
         storage = chrome.storage.local;
 
     mpp.isText = () => {
@@ -50,6 +51,25 @@
         }
     }
 
+    var buildCtx = (coll, k, level, ctx) => {
+        if (k >= coll.length || coll[k].level <= level) { return k; }
+        var node = coll[k];
+        ctx.push("<li><a href='#" + node.anchor + "'>" + node.text + "</a>");
+        k++;
+        var childCtx = [];
+        k = buildCtx(coll, k, node.level, childCtx);
+        if (childCtx.length > 0) {
+            ctx.push("<ul>");
+            childCtx.forEach(function (idm) {
+                ctx.push(idm);
+            });
+            ctx.push("</ul>");
+        }
+        ctx.push("</li>");
+        k = buildCtx(coll, k, level, ctx);
+        return k;
+    };
+
     // Onload, take the DOM of the page, get the markdown formatted text out and
     // apply the converter.
     function makeHtml(data) {
@@ -60,19 +80,45 @@
                 config.markedOptions.katex = true;
                 preHtml = diagramFlowSeq.prepareDiagram(preHtml);
             }
+
+            if (items.toc) {
+                const renderer = new marked.Renderer()
+                const slugger = new marked.Slugger()
+                const r = {
+                  heading: renderer.heading.bind(renderer),
+                };
+
+                renderer.heading = (text, level, raw, slugger) => {
+                    var anchor = config.markedOptions.headerPrefix + slugger.serialize(raw)
+
+                    toc.push({
+                        anchor: anchor,
+                        level: level,
+                        text: text
+                    });
+
+                    return r.heading(text, level, raw, slugger);
+                };
+                config.markedOptions.renderer = renderer;
+            }
+
             marked.setOptions(config.markedOptions);
             var html = marked(preHtml);
             html = DOMPurify.sanitize(html, {
                 ADD_ATTR: ['flow'],
                 SANITIZE_DOM: false
             });
+
+            if (items.toc) {
+                var ctx = [];
+                ctx.push('<div class="toc-list"><h1 id="table-of-contents">Table of Contents</h1>\n<ul>');
+                buildCtx(toc, 0, 0, ctx);
+                ctx.push("</ul></div>");
+                html = ctx.join('') + html
+            }
             $(document.body).html(html);
 
             $('img').on("error", () => resolveImg(this));
-
-            if (items.toc) {
-                addTOC();
-            }
 
             diagramFlowSeq.drawAllFlow();
             diagramFlowSeq.drawAllSeq();
@@ -246,279 +292,5 @@
             }
         }
     });
-
-    // {{{ Start of TOC code
-    var showNavBar = true;
-    var expandNavBar = true;
-    var hasNavItem = true;
-    var vH1Tag = null, vH2Tag = null, vH3Tag = null,
-        vH4Tag = null, vH5Tag = null, vH6Tag = null;
-    var headerNavs;
-    var headerTops = [];
-    var tocTops = [];
-
-    var scrollDirection = "none";
-    var lastY = 'undefined';
-    function scrollFunc() {
-        if (typeof lastY == 'undefined') {
-          lastY = window.pageYOffset;
-        }
-        var diffY = lastY - window.pageYOffset;
-        lastY = window.pageYOffset;
-        if (diffY < 0) { // down
-          scrollDirection = 'down';
-        } else if (diffY > 0) { // up
-          scrollDirection = 'up';
-        } else { // first enter
-          scrollDirection = 'first';//just set as "first"
-        }
-    };
-
-    function addTOC() {
-        makeTOC();
-        if(showNavBar && hasNavItem){
-            if(hasScroll("AnchorContent")){
-                $("#AnchorContent").hover(function (){
-                    $("#AnchorContent").preventScroll();
-                },function (){
-                });
-            }
-            setTimeout(calcBounds, 100);
-        }
-    };
-
-    $(window).resize(function(){
-        if(showNavBar){
-            var clientheight = document.compatMode=="CSS1Compat" ? document.documentElement.clientHeight : document.body.clientHeight;
-            $("#AnchorContent").css('max-height', (clientheight - 160) + 'px');
-        }
-    });
-
-    function makeTOC(){
-        var h1s = $("body").find("h1");
-        var h2s = $("body").find("h2");
-        var h3s = $("body").find("h3");
-        var h4s = $("body").find("h4");
-        var h5s = $("body").find("h5");
-        var h6s = $("body").find("h6");
-
-        var headCounts = [h1s.length, h2s.length, h3s.length, h4s.length, h5s.length, h6s.length];
-        for(var i = 0; i < headCounts.length; i++){
-            if(headCounts[i] > 0){
-                if (vH1Tag == null){
-                    vH1Tag = 'h' + (i + 1);
-                } else if(vH2Tag == null){
-                    vH2Tag = 'h' + (i + 1);
-                } else if(vH3Tag == null){
-                    vH3Tag = 'h' + (i + 1);
-                } else if(vH4Tag == null){
-                    vH4Tag = 'h' + (i + 1);
-                } else if(vH5Tag == null){
-                    vH5Tag = 'h' + (i + 1);
-                } else if(vH6Tag == null){
-                    vH6Tag = 'h' + (i + 1);
-                }
-            }
-        }
-
-        if(vH1Tag == null){
-            hasNavItem = false;//hide TOC, nothing to show
-            return;
-        }else{
-            hasNavItem = true;//reset to display TOC
-        }
-
-        if($(".BlogAnchor").length == 0){
-            $("body").append('<div class="BlogAnchor">' +
-                '<span style="color:red;position:absolute;top:-3px;left:3px;cursor:pointer;" ' +
-                'onclick="document.getElementsByClassName(\'BlogAnchor\')[0].style.display = \'none\';">X</span>' + '<p>' +
-                '<b id="AnchorContentToggle" title="Expand" style="cursor:pointer;">Content▲</b>' +
-                '</p>' + '<div class="AnchorContent" id="AnchorContent"> </div>' + '</div>' );
-            var clientheight = document.compatMode=="CSS1Compat" ?
-                    document.documentElement.clientHeight : document.body.clientHeight;
-            $("#AnchorContent").css('max-height', (clientheight - 160) + 'px');
-        }
-
-        var vH1Index = 0, vH2Index = 0, vH3Index = 0,
-            vH4Index = 0, vH5Index = 0, vH6Index = 0;
-        $("body").find("h1,h2,h3,h4,h5,h6").each(function(i,item){
-            var id = '';
-            var name = '';
-            var tag = $(item).get(0).tagName.toLowerCase();
-            var className = '';
-
-            if (tag == vH1Tag){
-                id = name = ++vH1Index;
-                name = id;
-                vH2Index = 0;
-                className = 'item_h1';
-            } else if(tag == vH2Tag){
-                id = vH1Index + '_' + ++vH2Index;
-                name = vH1Index + '.' + vH2Index;
-                vH3Index = 0;
-                className = 'item_h2';
-            } else if(tag == vH3Tag){
-                id = vH1Index + '_' + vH2Index + '_' + ++vH3Index;
-                name = vH1Index + '.' + vH2Index + '.' + vH3Index;
-                vH4Index = 0;
-                className = 'item_h3';
-            } else if(tag == vH4Tag){
-                id = vH1Index + '_' + vH2Index + '_' + vH3Index + '_' + ++vH4Index;
-                name = vH1Index + '.' + vH2Index + '.' + vH3Index + '.' + vH4Index;
-                className = 'item_h4';
-            } else if(tag == vH5Tag){
-                id = [vH1Index, vH2Index, vH3Index, vH4Index, ++vH5Index].join('_');
-                name = [vH1Index, vH2Index, vH3Index, vH4Index, vH5Index].join('.');
-                vH5Index = 0;
-                className = 'item_h5';
-            } else if(tag == vH6Tag){
-                id = [vH1Index, vH2Index, vH3Index, vH4Index, vH5Index, ++vH6Index].join('_');
-                name = [vH1Index, vH2Index, vH3Index, vH4Index, vH5Index, vH6Index].join('.');
-                vH6Index = 0;
-                className = 'item_h6';
-            }
-
-            $(item).attr("id","wow"+id);
-            $(item).addClass("wow_head");
-            $("#AnchorContent").append('<li><a class="nav_item '+className+' anchor-link" onclick="return false;" href="#" link="#wow'+id+'">'+name+"&ensp;"+$(this).text()+'</a></li>');
-        });
-
-        $("#AnchorContentToggle").click(function(){
-            var text = $(this).html();
-            if(text=="Content▲"){
-                $(this).html("Content▼");
-                $(this).attr({"title":"Collapse"});
-                $(".BlogAnchor").css("min-width","6%");
-            }else{
-                $(this).html("Content▲");
-                $(this).attr({"title":"Expand"});
-                $(".BlogAnchor").css("min-width","25%");
-            }
-            $("#AnchorContent").toggle();
-        });
-
-        $(".anchor-link").click(function(){
-            $(".BlogAnchor li .nav_item.current").removeClass('current');
-            $(this).addClass('current');
-            $(window).off('scroll', doScroll);
-            $("html,body").animate({scrollTop: $($(this).attr("link")).offset().top}, 100,
-                function(){ $(window).on('scroll', doScroll); });
-        });
-
-        if(!showNavBar){ $('.BlogAnchor').hide(); }
-        if(!expandNavBar){
-            $(this).html("Content▼");
-            $(this).attr({"title":"Collapse"});
-            $("#AnchorContent").hide();
-        }
-    }
-
-    function doScroll(){ //$(window).scroll
-        scrollFunc();
-        var scrollTop = $(window).scrollTop();
-        var clientheight = document.compatMode=="CSS1Compat" ? document.documentElement.clientHeight : document.body.clientHeight;
-        $.each(headerTops, function(i, n){
-            var distance = n - scrollTop;
-            if(distance > 0){
-                var item = $(headerNavs[i])[0];
-                var curItemTop = $(headerNavs[i]).position().top;
-                var dist2Top = distance + item.offsetHeight;
-
-                if(scrollDirection == 'first'){
-                    //handle first in
-                    if(dist2Top < clientheight){
-                        $(".BlogAnchor li .nav_item.current").removeClass('current');
-                        $(headerNavs[i]).addClass('current');
-                    }else{
-                        $(".BlogAnchor li .nav_item.current").removeClass('current');
-                        if(i > 0){
-                            $(headerNavs[i - 1]).addClass('current');
-                        }
-                    }
-                    return false;
-                }
-                if(dist2Top < clientheight){
-                    $(".BlogAnchor li .nav_item.current").removeClass('current');
-                    $(headerNavs[i]).addClass('current');
-                    return false;
-                }else if(scrollDirection == 'up'){
-                    if(item.classList.contains('current')==true){
-                        $(".BlogAnchor li .nav_item.current").removeClass('current');
-                        if(i > 0){
-                            $(headerNavs[i - 1]).addClass('current');
-                            return false;
-                        }
-                    }
-                }
-            }
-        });
-        adjustCurItem();
-    }
-
-    function adjustCurItem(){
-        var curItem = $(".BlogAnchor li .nav_item.current");
-        if(typeof curItem.get(0) == "undefined") {
-            return;
-        }
-
-        var curItemTop = curItem.position().top;
-        var curItemHeight = document.getElementById("AnchorContent").getElementsByClassName("current")[0].offsetHeight;
-        var tocTop = $("#AnchorContent").position().top;
-        var tocHeight = document.getElementById("AnchorContent").clientHeight;
-        var scrolltop = $("#AnchorContent").scrollTop();
-        if(curItemTop <= tocTop){ //currentItem showed in AnchorContent
-            var dist = tocTop - curItemTop;
-            if(dist > 0){ $("#AnchorContent").scrollTop(scrolltop - dist); }
-        }else if(curItemTop + curItemHeight > tocTop){
-            var dist = curItemTop + curItemHeight - tocTop - tocHeight;
-            if(dist > 0){ //currentItem beyond AnchorContent
-                $("#AnchorContent").scrollTop(dist + scrolltop);
-            }
-        }
-    }
-
-    function calcBounds(){
-        headerNavs = $(".BlogAnchor li .nav_item");
-        $(".wow_head").each(function(i, n){
-            headerTops.push($(n).offset().top);
-        });
-        headerNavs.each(function(i, n){
-            tocTops.push($(n).offset().top);
-        });
-
-        $(window).on('scroll', doScroll);
-        doScroll();
-    }
-
-    function hasScroll(Id){
-        var obj=document.getElementById(Id);
-        if(obj && obj.scrollHeight > obj.clientHeight){
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    //for preventDefault()
-    $.fn.extend({
-        "preventScroll":function(){
-            $(this).each(function(){
-                var _this = this;
-                if(navigator.userAgent.indexOf('Firefox') >= 0){   //firefox
-                    _this.addEventListener('DOMMouseScroll',function(e){
-                        _this.scrollTop += e.detail > 0 ? 50 : -50;
-                        e.preventDefault();
-                    },false);
-                }else{
-                    _this.onmousewheel = function(e){
-                        e = e || window.event;
-                        _this.scrollTop += e.wheelDelta > 0 ? -50 : 50;
-                        return false;
-                    };
-                }
-            })
-        }
-    });
-    // }}} End of TOC code
 
 }(document));
