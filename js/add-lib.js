e7c9c1fae481c55dfdc6855244cd2dd4b2c7d079
yanc <volcas@gmail.com>
2022-12-01 16:47:35 +0800

    update mermaid to v9.2.2

--- js/add-lib.js
+++ js/add-lib.js
@@ -2,4 +2,4 @@ const { execSync } = require('child_process')
 
 execSync("npx copyfiles -f node_modules/jquery/dist/jquery.js js/")
 execSync("npx copyfiles -f node_modules/marked/marked.min.js js/")
-execSync("npx copyfiles -f node_modules/mermaid/mermaid.min.js js/")
+execSync("npx copyfiles -f node_modules/mermaid/dist/mermaid.min.js js/")
