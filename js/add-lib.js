78d096e02327f9947ef0465076a9c9f5c8bfffad
yanc <volcas@gmail.com>
2023-05-25 10:54:54 +0800

    see #146 fix path

--- js/add-lib.js
+++ js/add-lib.js
@@ -1,6 +1,6 @@
 const { execSync } = require('child_process')
 
 execSync("npx copyfiles -f node_modules/jquery/dist/jquery.js js/")
-execSync("npx copyfiles -f node_modules/marked/lib/marked.min.js js/")
+execSync("npx copyfiles -f node_modules/marked/marked.min.js js/")
 execSync("npx copyfiles -f node_modules/marked-highlight/lib/index.cjs js/marked-highlight")
 execSync("npx copyfiles -f node_modules/mermaid/dist/mermaid.min.js js/")
